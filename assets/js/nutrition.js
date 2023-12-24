114ec1f93332e387f3754b88b8091b11c8c41373
EYUP <jobmoonday2@gmail.com>
2023-12-24 22:21:56 +0000

    wrote some code to link API data to nutrition values page

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -1,6 +1,15 @@
 var nutriEndPoint = "https://api.edamam.com/api/nutrition-data";
 var appKey = "17bd128ddf8ca68645f972ccdade2f3a";
 var appId = "b30b69ec"
+var caloriesTotal=1000;
+var calciumTotal=0;
+
+/* 
+: 
+CA
+: 
+{label: 'Calcium, Ca', quantity: 57, unit: 'mg'}
+ */
 
 
 var recipeIngredients=[];
@@ -13,7 +22,15 @@ function getData(callSearchWord) {
         .then(function (data) {
           return data.json();
         }).then(function(data) {
-            console.log(data);
+            // console.log(data)
+            caloriesTotal+=data.calories
+            console.log(caloriesTotal)
+           
+          
+        $("#calcium").text("Calories: "+caloriesTotal)
+
+            calciumTotal+=data.totalNutrients.CA.quantity
+            console.log(calciumTotal)
     });
 
 
@@ -38,20 +55,31 @@ function getArray() {
 $("#submitBtn").on("click", function() {
     
       getArray();
-      console.log(recipeIngredients)
+      //console.log(recipeIngredients)
 
     for(i=0;i<recipeIngredients.length;i++){
 
         getData(recipeIngredients[i]);
+      
 
     }
 
 });
 
+$("#clearBtn").on("click", function() {
+    
+  $("#demo").text("");
+
+});
 /* 
 <textarea name="" id="demo" cols="30" rows="10" class="form-control">1 cup rice, 100 grams chickpeas</textarea>
-</p>
+
 <button type="button" id="submitBtn" class="btn btn-green px-5 calc-analysis-api"> Submit</button>
+<button type="button" id="clearBtn" class="btn btn-green px-5 calc-analysis-api"> Clear</button>
+
+<ul id="nutriList"> 
+    <li id="calcium"></li>
+</ul>
  */
 
 
