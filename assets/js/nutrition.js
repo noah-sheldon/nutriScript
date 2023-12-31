e4b81b7fc930d5a6d40b18fbd9debf4eac53ac05
Noah Sheldon <noahsheldon06@gmail.com>
2023-12-31 19:21:18 +0000

    fix duplication of table, added nutritionDiv to index.html

--- assets/js/nutrition.js
+++ assets/js/nutrition.js
@@ -6,109 +6,116 @@ var weightTotal = 0;
 var recipeIngredients = [];
 
 function getData(callSearchWord) {
-    var queryURL = "" + nutriEndPoint + "?app_id=" + appId + "&app_key=" + appKey + "&nutrition-type=cooking&ingr=" + callSearchWord + "";
+  var queryURL =
+    "" +
+    nutriEndPoint +
+    "?app_id=" +
+    appId +
+    "&app_key=" +
+    appKey +
+    "&nutrition-type=cooking&ingr=" +
+    callSearchWord +
+    "";
 
-    fetch(queryURL)
-        .then(function (data) {
-            return data.json();
-        }).then(function (data) {
-            console.log(data);
-            getSummaryInfo(data);
-        });
+  fetch(queryURL)
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      getSummaryInfo(data);
+    });
 }
 
 function getSummaryInfo(inputData) {
-    var tableRow = $('<tr>');
+  var tableRow = $("<tr>");
 
-    const firstValue = Object.values(inputData.ingredients[0])[0];
-    $("#nutriTbody").append(tableRow)
-        .append("<td>" + firstValue + "</td>")
-        .append("<td>" + inputData.calories + " kcal</td>")
-        .append("<td>" + (inputData.totalWeight).toFixed(0) + " grams</td>");
+  const firstValue = Object.values(inputData.ingredients[0])[0];
+  $("#nutriTbody")
+    .append(tableRow)
+    .append("<td>" + firstValue + "</td>")
+    .append("<td>" + inputData.calories + " kcal</td>")
+    .append("<td>" + inputData.totalWeight.toFixed(0) + " grams</td>");
 
-    // Update the total calories and weight
-    caloriesTotal += inputData.calories;
-    weightTotal += inputData.totalWeight;
+  // Update the total calories and weight
+  caloriesTotal += inputData.calories;
+  weightTotal += inputData.totalWeight;
 
-    // Update the totals row
-    updateTotalsRow();
+  // Update the totals row
+  updateTotalsRow();
 }
 
 function updateTotalsRow() {
-    // Remove the existing totals row
-    $("#totalsRow").remove();
+  // Remove the existing totals row
+  $("#totalsRow").remove();
 
-    // Create a new totals row
-    var totalsRow = $('<tr class="table-secondary" id="totalsRow">')
-        .append("<th>Total</th>")
-        .append("<th>" + caloriesTotal.toFixed(0) + " KCAL</th>")
-        .append("<th>" + weightTotal.toFixed(0) + " grams</th>");
+  // Create a new totals row
+  var totalsRow = $('<tr class="table-secondary" id="totalsRow">')
+    .append("<th>Total</th>")
+    .append("<th>" + caloriesTotal.toFixed(0) + " KCAL</th>")
+    .append("<th>" + weightTotal.toFixed(0) + " grams</th>");
 
-    // Append the totals row to the table body
-    $("#nutriTbody").append(totalsRow);
+  // Append the totals row to the table body
+  $("#nutriTbody").append(totalsRow);
 }
 
 function getArray() {
-    // Get the content of the textarea
-    var str = document.getElementById("ingredientTextArea").value;
+  $("#nutritionDiv").empty();
+  // Get the content of the textarea
+  var str = document.getElementById("ingredientTextArea").value;
 
-    if(!str){
-      alert('Please enter some ingredients!');
-      return;
-    }
+  if (!str) {
+    alert("Please enter some ingredients!");
+    return;
+  }
 
-    if (!str.includes(",")){
-      alert('Please add commas between each ingredient!')
-      return;
-    }
+  if (!str.includes(",")) {
+    alert("Please add commas between each ingredient!");
+    return;
+  }
 
-    // Split the content into an array using commas as the delimiter
-    recipeIngredients = str.split(',');
+  // Split the content into an array using commas as the delimiter
+  recipeIngredients = str.split(",");
 
-    // Trim each string to remove whitespaces
-    recipeIngredients = recipeIngredients.map(function (str) {
-        return str.trim();
-    });
+  // Trim each string to remove whitespaces
+  recipeIngredients = recipeIngredients.map(function (str) {
+    return str.trim();
+  });
 
-    
+  createTable();
+
+  for (let i = 0; i < recipeIngredients.length; i++) {
+    getData(recipeIngredients[i]);
+  }
 }
 
 function createTable() {
-    var nutriDiv = $("<div class='nutri-result'>");
-    var nutriDiv2 = $("<div class='col-md-12'>");
+  var nutriDiv = $("<div class='nutri-result'>");
+  var nutriDiv2 = $("<div class='col-md-12'>");
 
-    // create table
-    var table = $("<table class='table table-striped' style='margin-top: 30px; border: 1px solid #808080;'>");
+  // create table
+  var table = $(
+    "<table class='table table-striped' style='margin-top: 30px; border: 1px solid #808080;'>"
+  );
 
-    // table header
-    var headerRow = $("<tr>")
-        .append("<th scope='col' style='width: 40%;'>Quantity / Unit / Food</th>")
-        .append("<th scope='col' style='width: 30%;'>Calories</th>")
-        .append("<th scope='col' style='width: 30%;'>Weight</th>");
+  // table header
+  var headerRow = $("<tr>")
+    .append("<th scope='col' style='width: 40%;'>Quantity / Unit / Food</th>")
+    .append("<th scope='col' style='width: 30%;'>Calories</th>")
+    .append("<th scope='col' style='width: 30%;'>Weight</th>");
 
-    $("<thead class='table-secondary'>").append(headerRow).appendTo(table);
+  $("<thead class='table-secondary'>").append(headerRow).appendTo(table);
 
-    // table body
-    var tableBody = $("<tbody id='nutriTbody'>");
-    table.append(tableBody);
+  // table body
+  var tableBody = $("<tbody id='nutriTbody'>");
+  table.append(tableBody);
 
-    // append the table to the nutriForm
-    $("#nutriForm").append(nutriDiv).append(nutriDiv2).append(table);
+  // append the table to the nutriForm
+  $("#nutritionDiv").append(nutriDiv).append(nutriDiv2).append(table);
 }
 
 $("#submitBtn").click(function (event) {
-    event.preventDefault();
-
-    getArray();
-    console.log(recipeIngredients);
-
-    createTable();
-
-    for (let i = 0; i < recipeIngredients.length; i++) {
-        getData(recipeIngredients[i]);
-    }
+  event.preventDefault();
+  getArray();
+  console.log(recipeIngredients);
 });
-
-
-
-
